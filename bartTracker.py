2ec3169d2f98481afa82d2dd5916b5a32ea5d34f
Michael Salaverry <barakplasma@gmail.com>
2009-05-07 15:13:11 -0700

    created a bunch of objects for the various gtfs files for bart

--- bartTracker.py
+++ bartTracker.py
@@ -1,6 +1,17 @@
 print "gogo gadget"
 agency = open('agency.txt','r')
-print agency
-print agency.read()
+stops = open('stops.txt','r')
+routes = open('routes.txt','r')
+trips = open('trips.txt','r')
+stop_times = open('stop_times.txt','r')
+calendar = open('calendar.txt','r')
+calendar_dates = open('calendar_dates.txt','r')
+fare_attributes = open('fare_attributes.txt','r')
+fare_rules = open('fare_rules.txt','r')
+shapes = open('shapes.txt','r')
+frequencies = open('frequencies.txt','r')
+transfers = open('transfers.txt','r')
+#print agency
+print agency.readline()
 agency.close()
-print agency
+#print agency
